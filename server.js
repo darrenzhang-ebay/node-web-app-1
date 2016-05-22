52c9e1cb2fb25bb1475627fc5287d6dac3caa432
darrenzhang-ebay <dazhang@ebay.com>
2016-05-22 09:57:58 -0700

    branch0.1

--- server.js
+++ server.js
@@ -70,6 +70,6 @@ var server = app.listen(8081, function () {
     var host = server.address().address
     var port = server.address().port
 
-    console.log("Example app listening at http://%s:%s", host, port)
+    console.log("ARE YOU KIDDING? Example app listening at http://%s:%s", host, port)
 
 })
