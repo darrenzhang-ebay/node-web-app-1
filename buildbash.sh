e590dddbf189ec6f06f6d95cdeb58f28462dc90b
darrenzhang-ebay <dazhang@ebay.com>
2016-06-10 10:42:59 -0600

    0.8.1

--- buildbash.sh
+++ buildbash.sh
@@ -2,10 +2,20 @@
 
 set -o pipefail
 
+echo "Clearing old images..."
+docker rmi $(docker images -q -f dangling=true)
+docker rm -f(docker ps -a -q)
+docker r
+
 IMAGE="dazhang/node-web-app-1"
-VERSION="0.8"
+VERSION="0.8.1"
 
 echo ${VERSION}
+
+echo "Clearing old images..."
+docker rmi $(docker images -q -f dangling=true)
+docker rm -f(docker ps -a -q)
+docker rmi $(docker images -q | grep ${IMAGE})
 
 echo "Start building"
 docker build -t ${IMAGE}:${VERSION} . | sudo tee build.log || exit 1
@@ -17,6 +27,10 @@ ID=$(tail -1 build.log | awk '{print $3;}')
 echo "Tagging the latest"
 docker tag $ID ${IMAGE}:latest
 
+echo "pushing the latest to hub"
+docker push ${IMAGE}:${VERSION}
+
+echo "End"
 
 # docker images | grep ${IMAGE}
 # docker run --rm ${IMAGE}:latest /opt/java7/bin/java -version
