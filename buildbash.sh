e6158248af2b1aa171f156f720e3b978203819a6
darrenzhang-ebay <dazhang@ebay.com>
2016-06-10 10:23:04 -0600

    0.8

--- buildbash.sh
+++ buildbash.sh
@@ -3,13 +3,20 @@
 set -o pipefail
 
 IMAGE="dazhang/node-web-app-1"
-VERSION="0.7"
+VERSION="0.8"
 
-docker build -t ${IMAGE}:${VERSION} . | tee build.log || exit 1
+echo ${VERSION}
+
+echo "Start building"
+docker build -t ${IMAGE}:${VERSION} . | sudo tee build.log || exit 1
+echo "End building"
+
+echo "Get the latest ID"
 ID=$(tail -1 build.log | awk '{print $3;}')
 
+echo "Tagging the latest"
 docker tag $ID ${IMAGE}:latest
 
-docker images | grep ${IMAGE}
 
+# docker images | grep ${IMAGE}
 # docker run --rm ${IMAGE}:latest /opt/java7/bin/java -version
