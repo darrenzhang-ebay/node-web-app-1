9302abe033ea495c6aea737719ef3a74e82ab0db
darrenzhang-ebay <dazhang@ebay.com>
2016-06-14 10:37:46 -0600

    0.8.1.10

--- buildbash.sh
+++ buildbash.sh
@@ -3,7 +3,7 @@
 set -o pipefail
 
 IMAGE="dazhang/node-web-app-1"
-VERSION="0.8.1.9"
+VERSION="0.8.1.10"
 
 echo ${VERSION}
 echo
--- installdocker.sh
+++ installdocker.sh
@@ -1,35 +1,45 @@
-#!/usr/bin/env bash
+#!usr/bin/env bash
 
 set -o pipefail
 
-echo "========================="
-echo "Update the package information"
-echo "========================="
-echo ""
-/usr/bin/apt-get update
-/usr/bin/apt-get install apt-transport-https ca-certificates
-# echo "Adding the new GPG key"
-# sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+echo "=================================="
+echo "Update the package information..."
+echo "=================================="
+apt-get update
+apt-get install apt-transport-https ca-certificates <<-EOF
+yes
+EOF
 
-echo "========================="
-echo "Update the source entry"
-echo "========================="
-echo ""
+echo "=================================="
+echo "Adding public key..."
+echo "=================================="
+apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+
+echo "=================================="
+echo "Update the source entry..."
+echo "=================================="
 cd /etc/apt/sources.list.d/
+rm docker.list <<-EOA
+yes
+EOA
 touch docker.list
-echo "deb https://apt.dockerproject.org/repo ubuntu-xenial main" > docker.list
+echo "deb https://apt.dockerproject.org/repo ubuntu-xenial main" >docker.list
+apt-get update
+apt-get purge lxc-docker
+apt-cache policy docker-engine
 
-echo "========================="
-echo "Install Docker"
-echo "========================="
-echo ""
-/usr/bin/apt-get update /usr/bin/apt-get install docker-engine
+echo "=================================="
+echo "install docker..."
+echo "=================================="
+apt-get update
+apt-get install linux-image-extra-$(uname -r)
+apt-get install docker-engine <<-EOB
+yes
+EOB
 service docker start
 
-echo "========================="
-echo "Create a docker group and add to the group"
-echo "========================="
-echo ""
+echo "=================================="
+echo "create docker group and add sysadmin to the group..."
+echo "=================================="
 groupadd docker
 usermod -aG docker sysadmin
-
